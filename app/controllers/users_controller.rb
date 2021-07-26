9c8673459a670492ca215bb9482fa0ecf15bef17
Gerald Bivens <geraldbivens@gmail.com>
2021-07-26 08:35:20 -0600

    Add sigin action and route.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,4 +13,19 @@ class UsersController < ApplicationController
 
         render json: @user, status: :created
     end
+
+    def signin
+        @user = User.find_by(username: params[:username])
+
+        if @user && @user.authenticate(params[:password])
+            payload = { user_id: @user.id }
+
+            # @token = JWT.encode( payload, Rails.application.secret_key_base[0] )
+            @token = JWT.encode( payload, 'secret' )
+
+            render json: { user: @user, token: @token }
+        else
+            render json: { error: 'Invalid credentials' }, status: :unauthorized
+        end
+    end
 end
